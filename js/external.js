d09b9aa6566cf958286c2e2fdb35c7790c9330c7
Anwar Lopez <anwarlo247@gmail.com>
2019-02-06 15:35:59 -0600

    added files and code for first half of JS with HTML exercise

---
+++ js/external.js
@@ -0,0 +1,4 @@
+console.log("Hello from external Javascript")
+alert("Welcome to my Website!")
+var color=prompt("What is your favorite color?")
+alert(("Great, ")+color+(" is my favorite color too!"))
